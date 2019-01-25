cf8c42f43036fb5c3ecc239ec5edc1f5f98d4e46
luoxuesen <luoxuesen@ysstech.com>
2019-01-25 14:25:03 +0800

    Initial commit

---
+++ src/main/java/com/luo/ribbon/ribbon/RibbomApplication.java
@@ -0,0 +1,28 @@
+package com.luo.ribbon.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+public class RibbomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbomApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+
+}
+
